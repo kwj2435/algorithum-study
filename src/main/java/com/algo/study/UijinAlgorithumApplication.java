69eca605be55dda4d84d2a9d3962e923d7dfc4b3
kwj2435 <>
2021-12-09 15:40:27 +0900

    first

---
+++ src/main/java/com/algo/study/UijinAlgorithumApplication.java
@@ -0,0 +1,13 @@
+package com.algo.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UijinAlgorithumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UijinAlgorithumApplication.class, args);
+	}
+
+}
