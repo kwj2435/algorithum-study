4a09f8b7da5e179d8211df4b8c8fb0a91e2a9db7
kwj2435 <dmlwls2435@naver.com>
2021-12-09 15:55:11 +0900

    first

--- src/main/java/com/algo/study/UijinAlgorithumApplication.java
+++ src/main/java/com/algo/study/UijinAlgorithumApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UijinAlgorithumApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UijinAlgorithumApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(UijinAlgorithumApplication.class, args);
+    }
 
 }
--- src/test/java/com/algo/study/UijinAlgorithumApplicationTests.java
+++ src/test/java/com/algo/study/UijinAlgorithumApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class UijinAlgorithumApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
