30d5decb71e6970ed346a1254cfc6840c7e21964
kimuijin <dmlwls2435@naver.com>
2022-12-13 17:33:22 +0900

    Initial commit

---
+++ src/main/kotlin/com/algo/algorithumstudy/AlgorithumStudyApplication.kt
@@ -0,0 +1,11 @@
+package com.algo.algorithumstudy
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AlgorithumStudyApplication
+
+fun main(args: Array<String>) {
+    runApplication<AlgorithumStudyApplication>(*args)
+}
---
+++ src/test/kotlin/com/algo/algorithumstudy/AlgorithumStudyApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.algo.algorithumstudy
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AlgorithumStudyApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
