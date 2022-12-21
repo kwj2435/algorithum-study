1397e64daee932ec4c5d5cc5f53e90e1005d0cbe
kimuijin <dmlwls2435@naver.com>
2022-12-21 21:09:50 +0900

    [add] codility PermCheck

---
+++ codility/PermCheck.kt
@@ -0,0 +1,14 @@
+package com.algo.programmers.level01
+
+// BinaryGap
+class BinaryGap {
+    fun solution(A: IntArray): Int {
+        for(value in A) {
+            if(value > A.size) break
+            arr[value] = 1
+        }
+
+        return if(arr.filter { it == 1 }.sum() == A.size) 1 else 0
+    }
+}
+
