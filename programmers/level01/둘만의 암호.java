a477ecfa3af561d3033e6aad6b4242511435a7bc
UiJin Kim <dmlwls2435@naver.com>
2023-02-22 09:39:52 +0900

    add programmers level01

--- programmers/level01/둘만의 암호.java
+++ programmers/level01/둘만의 암호.java
@@ -1,1 +1,24 @@
+import java.util.*;
 
+// 둘만의 암호
+// https://school.programmers.co.kr/learn/courses/30/lessons/155652
+class P155652 {
+   class Solution {
+    public String solution(String s, String skip, int index) {
+        var sb = new StringBuilder();
+
+        for(char c: s.toCharArray()) {
+            var i = 0;
+            while(i<index) {
+                c++;
+                if(c == 123) c = 97;
+                if(skip.indexOf(c) == -1) {
+                    i++;
+                }
+            }
+            sb.append(c);
+        }
+
+        return sb.toString();
+    }
+}
