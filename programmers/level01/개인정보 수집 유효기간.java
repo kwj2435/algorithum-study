c36a9183df9704fe8827839468164ab6acad53d9
UiJin Kim <dmlwls2435@naver.com>
2023-02-23 10:10:25 +0900

    [ADD] Programmers level1

--- programmers/level01/개인정보 수집 유효기간.java
+++ programmers/level01/개인정보 수집 유효기간.java
@@ -1,1 +1,31 @@
+// 개인정보 수집 유효기간
+// https://school.programmers.co.kr/learn/courses/30/lessons/150370
+class P150370 {
+    public int[] solution(String today, String[] terms, String[] privacies) {
+        List<Integer> answer = new LinkedList();
+        String[] splitToday = today.split("\\.");
+        Map<String, String> termsMap = new HashMap();
 
+        for(String term: terms) {
+           String[] splitTerm = term.split(" ") ;
+           termsMap.put(splitTerm[0], splitTerm[1]);
+        }
+
+        int idx = 1;
+        for(String privacy: privacies) {
+            String[] splitPrivacy = privacy.split(" ");
+            String[] splitPrivacyDate = splitPrivacy[0].split("\\.");
+
+            int elapsedTime = (Integer.parseInt(splitToday[0]) - Integer.parseInt(splitPrivacyDate[0]))*336;
+            elapsedTime += (Integer.parseInt(splitToday[1]) - Integer.parseInt(splitPrivacyDate[1]))*28;
+            elapsedTime += Integer.parseInt(splitToday[2]) - Integer.parseInt(splitPrivacyDate[2]);
+
+            if(elapsedTime >= Integer.parseInt(termsMap.get(splitPrivacy[1]))*28) {
+                answer.add(idx);
+            }
+            idx++;
+        }
+
+        return answer.stream().mapToInt(i -> i).toArray();
+    }
+}
